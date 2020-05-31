1099cab2ea761894fcdf66a5fd488418e6cc57ae
rayenok <sk0l3m@gmail.com>
2020-05-31 07:54:06 +0100

    added new dependencies for i3

--- install-scripts/basic/i3.sh
+++ install-scripts/basic/i3.sh
@@ -1,4 +1,10 @@
-sudo apt install jq i3 feh nitrogen compton terminology
+sudo apt install jq i3 feh nitrogen compton terminology zenity rofi python3
+
+# installing this for having graphical interface when setting/going to marks
+pip3 install git+https://github.com/talwrii/i3-rofi-mark#egg=i3-rofi-mark
+
+# installing i3wsr
+cargo install i3wsr
 
 # i3
 echo "Configuring i3"
