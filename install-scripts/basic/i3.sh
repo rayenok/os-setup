22147445221ec720fdd69954a9009a84dbd76262
rayenok <sk0l3m@gmail.com>
2020-07-13 09:56:17 +0200

    revamp i3 configuration (not tested)

--- install-scripts/basic/i3.sh
+++ install-scripts/basic/i3.sh
@@ -1,32 +1,11 @@
-sudo apt install jq i3 feh nitrogen compton terminology zenity rofi python3 python3-pip curl libxcb1-dev udiskie  volumeicon-alsa
+# installing oh-my-zsh
+source ./zsh.sh
 
-# install rust/cargo if not alreay installed
-if ! [ -x "$(command -v cargo)" ]; then
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	echo "Need to reboot before proceeding..."
-	echo "Reboot and execute this script again"
-else
+sudo add-apt-repository ppa:regolith-linux/release
+sudo apt install regolith-desktop regolith-look-ayu-dark i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time
 
-	# install rust
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+regolith-look set ayu-dark
+regolith-look refresh
 
-	# installing i3wsr
-	cargo install i3wsr
-
-	# installing oh-my-zsh
-	source ./zsh.sh
-
-	# i3
-	echo "Configuring i3"
-	if [ -e ~/.i3 ]; then
-		echo "Deleting i3 directory"
-		rm -rf ~/.i3
-	fi
-	if [ -e $dotfilesdir/config-files/i3 ];then
-		if [ -e $USER/.i3 ]; then
-			rm -rf $USER/.i3
-		fi
-		ln -s $dotfilesdir/config-files/.i3 /home/$USER/.i3
-	fi
-fi
-
+mkdir /home/$USER/.config/regolith/i3
+ln -s /opt/dotfiles/.regolith /home/$USER/.config/regolith/i3/config
